715e8e754a57043abeb0292ffb4dd42016fbcf24
Marilyn Wren <Marilynwren64@yahoo.com>
2022-01-08 12:08:36 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+
+function introduction(name)
+ {  
+    return `Hi, my name is ${name}.`;
+  }
+ 
+
+  function introductionWithLanguage(name,language) 
+  { 
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+    
+   }
+
+
+   function introductionWithLanguageOptional(name,language="JavaScript") 
+   {   
+      console.log(`${language}="JavaScript"`);
+      return `"Hi, my name is ${name} and I am learning to program in ${language}."`;
+
+   }
+
+  function introductionWithLanguageOptional(name,language="JavaScript") 
+  {  
+     return`Hi, my name is ${name} and I am learning to program in ${language}.`;
+     
+  }
+   
+
+  
+   
+  
